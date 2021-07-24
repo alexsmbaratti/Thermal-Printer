d7d94011f50d407e93e072fb41aa1fed25adefce
Alex Baratti <alexsmbaratti@gmail.com>
2021-07-23 21:38:26 -0700

    Returns status based on paper status

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def hello_world():
     if print_utils.has_paper():
         return str(200)
     else:
-        return str(410) # Out of paper status code
+        return str(410)  # Out of paper status code
 
 
 @app.route('/api/pixel-shelf/library/size', methods=['POST'])
@@ -19,6 +19,12 @@ def print_library_count():
     count = api_utils.getLibraryCount()
     print_utils.print_library_size(count)
     return str(count)
+
+
+@app.route('/api/pixel-shelf/library', methods=['POST'])
+def print_library_entry():
+    # TODO: Read request body and print a library entry based on the request body
+    return str(501)
 
 
 @app.route('/api/pixel-shelf/library/<id>', methods=['POST'])
